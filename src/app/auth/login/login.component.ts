eb83307ce941618e77aaea3796a7cc34f687e1b3
cinta <cintatafur@gmail.com>
2022-09-13 09:13:12 +0200

    formateo codigo

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,51 +5,43 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-//es la variable inicializada del ngModel
- 
+  //es la variable inicializada del ngModel
+
   /* form es el nombre del evento del formulario 
       es un obj de tipo FormGroup y inicializo funcion statica donde valido el formulario*/
   public form: FormGroup = new FormGroup({
-    
-    email: new FormControl("user@email.com", [
+    email: new FormControl('user@email.com', [
       Validators.required,
       Validators.email,
-      Validators.maxLength(150)
+      Validators.maxLength(150),
     ]),
-    password: new FormControl("",  [
+    password: new FormControl('', [
       Validators.required,
       Validators.minLength(5),
-      Validators.maxLength(150)
-    ]) 
-  })
+      Validators.maxLength(150),
+    ]),
+  });
 
-  constructor(private router: Router ) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-
     const obj = {
-      email : "user@.angular.com",
-      password: "1234"
-    }
-   /*  this.form.patchValue(obj) */
+      email: 'user@.angular.com',
+      password: '1234',
+    };
+    /*  this.form.patchValue(obj) */
   }
-
 
   saveForm = () => {
-    const values = this.form.value
+    const values = this.form.value;
     console.log('saveForm', values);
-
-   
-  }
-  goRegister = () => {
-
-  }
-
-    //este es el evento del btn de ir que le lleva al login
-    goRecovery = () => {
-      this.router.navigate(['/auth/recovery']);
-    }
+  };
+  goRegister = () => {};
+  //este es el evento del btn de ir que le lleva al login
+  goRecovery = () => {
+    this.router.navigate(['/auth/recovery']);
+  };
 }
--- src/app/auth/recovery/recovery.component.ts
+++ src/app/auth/recovery/recovery.component.ts
@@ -5,37 +5,33 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-recovery',
   templateUrl: './recovery.component.html',
-  styleUrls: ['./recovery.component.scss']
+  styleUrls: ['./recovery.component.scss'],
 })
 export class RecoveryComponent implements OnInit {
-
-   /* form es el nombre del evento del formulario 
+  /* form es el nombre del evento del formulario 
       es un obj de tipo FormGroup y inicializo funcion statica donde valido el formulario*/
-      email:string= ''
-      public form: FormGroup = new FormGroup({
-    
-        email: new FormControl("", [
-          Validators.required,
-          Validators.email,
-          Validators.maxLength(150)
-        ])
-      })
-  constructor(private router:Router) { }
+  email: string = '';
+  public form: FormGroup = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.maxLength(150),
+    ]),
+  });
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  saveForm = (value:any) => {
-    const values = this.form.value
-    console.log('saveForm', values); 
-
-  }
+  saveForm = (value: any) => {
+    const values = this.form.value;
+    console.log('saveForm', values);
+  };
 
   //este es el evento del btn de ir que le lleva al login
   goLogin = () => {
     this.router.navigate(['/auth/login']);
-  }
+  };
   doRecovery = () => {
-    console.log('Recoveyr', this.email)
-  }
+    console.log('Recoveyr', this.email);
+  };
 }
