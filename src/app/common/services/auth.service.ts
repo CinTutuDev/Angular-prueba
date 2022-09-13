d66143b1d6910fce6c0164925a2091b77e43f94e
cinta <cintatafur@gmail.com>
2022-09-13 11:28:56 +0200

    �  funciones sistema login

--- src/app/common/services/auth.service.ts
+++ src/app/common/services/auth.service.ts
@@ -2,9 +2,24 @@ import { Injectable } from '@angular/core';
 
 //es para que pueda ser importada
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
 
-  constructor() { }
+  static currentUser = null
+
+  constructor() {}
+
+  //creo funciones de sistemas de login
+  login = (user: any) => {
+    AuthService.currentUser = user
+  }
+
+  logout = () => {
+    AuthService.currentUser = null
+  }
+
+  isLoged = ():boolean => {
+    return ( AuthService.currentUser === null)
+  }
 }
